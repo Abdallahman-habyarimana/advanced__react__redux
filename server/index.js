d8cafb0055f05ad499d1dfca4bfd60b8e7e1b2f3
Abdallahman Habyarimana <habdallah005@gmail.com>
2021-05-17 17:41:25 -0400

    Creating user model and hash password

--- server/index.js
+++ server/index.js
@@ -2,11 +2,22 @@ const express = require('express');
 const colors = require('colors')
 const http = require('http');
 const bodyParser = require('body-parser');
-const morgan = require('morgan')
+const morgan = require('morgan');
+const mongoose = require('mongoose')
 
-const app = express()
+const app = express();
+const router = require('./routers/router')
+
+//DB Setup
+mongoose.connect('mongodb://localhost/auth', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
 
 // Add Setup
+app.use(morgan('combined'))
+app.use(bodyParser.json({ type: '*/*'}))
+router(app)
 
 // Server Setup
 const port = process.env.PORT || 3090;
---
+++ server/routers/router.js
@@ -0,0 +1,4 @@
+const  Auth = require('../controllers/auth')
+module.exports = function(app) {
+    app.post('/signup', Auth.signup)
+}
