d66341e43d8dbd2d4373a04027b5ee6e3e000e43
Abdallahman Habyarimana <habdallah005@gmail.com>
2021-05-17 23:40:03 -0400

    Implementing passwords

--- server/routers/router.js
+++ server/routers/router.js
@@ -1,4 +1,12 @@
 const  Auth = require('../controllers/auth')
+const passportService = require('../services/passport')
+const passport = require('passport');
+
+const requireAuth = passport.authenticate('jwt', { session: false})
+
 module.exports = function(app) {
+    app.get('/', requireAuth, function(req, res) {
+        res.send({ 'hi': 'there'})
+    })
     app.post('/signup', Auth.signup)
 }
